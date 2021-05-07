19441e1aa1cc790259f339a6049b4ac093ca328c
Jelena <47182797+JDugonjic@users.noreply.github.com>
2021-05-07 16:43:53 +0200

    First commit, LoginScreen added

---
+++ screens/LoginScreen.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button, Input, Image } from 'react-native-elements';
+import { KeyboardAvoidingView } from 'react-native';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = () => {};
+
+  return (
+    <KeyboardAvoidingView behavior='padding' style={styles.container}>
+      <StatusBar style='light' />
+      <Image
+        source={{
+          uri:
+            'https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png',
+        }}
+        style={{
+          width: 200,
+          height: 200,
+        }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder='Email'
+          autoFocus
+          type='Email'
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder='Password'
+          secureTextEntry
+          type='password'
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <Button containerStyle={styles.button} title='Login' onPress={signIn} />
+      <Button containerStyle={styles.button} type='outline' title='Register' />
+      <View style={{ height: 10 }} />
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  inputContainer: {
+    width: 300,
+  },
+  button: {
+    width: 200,
+    marginTop: 10,
+  },
+});
